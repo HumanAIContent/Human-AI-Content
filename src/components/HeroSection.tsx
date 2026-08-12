83f2d7d722928d66cf1cc1b61b3512a5c01bb2dd
HumanAIContent <drac.munyang@gmail.com>
2026-08-12 21:25:00 +0800

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -59,7 +59,7 @@ export const HeroSection: React.FC<HeroSectionProps> = ({ onNavigate, onOpenSamp
               </div>
 
               {/* Centered Primary Action Button */}
-              <div className="flex justify-center my-8">
+              <div className="flex justify-center mt-8 mb-4">
                 <button
                   onClick={() => {
                     const el = document.getElementById('packages');
